a64d9f6840024f06b92d1684e576164ff4a989c4
tsxepo <tsxepo@gmail.com>
2024-09-15 00:51:58 +0200

    remove biceps

--- Program.cs
+++ Program.cs
@@ -1,13 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddApplicationInsightsTelemetry();
+
+builder.Services.AddMvc();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
